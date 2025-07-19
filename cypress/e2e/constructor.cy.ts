588ec7cf5aaa7a77c9b7a243564f6c05351fa949
Misha Kokorev <mishkokorev35@gmail.com>
2025-07-19 15:21:24 +0500

    1.3.0 :: исправление селекторов в тестах конструктора и baseUrl в cypress.config.ts

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,62 +1,66 @@
 import * as orderFixture from '../fixtures/order.json';
 
-const TEST_URL = 'http://localhost:4000';
 const dataCyBun = '[data-cy="bun"]';
 const dataCyBunFirst = '[data-cy="bun"]:first-of-type';
+const dataCyMain = '[data-cy="main"]';
+const dataCyMainFirst = '[data-cy="main"]:first-of-type';
+const dataCySauce = '[data-cy="sauce"]';
+const dataCySauceFirst = '[data-cy="sauce"]:first-of-type';
 const dataCyOrder = '[data-cy-order]';
+const selectorConstructorElementTop = 'div.constructor-element_pos_top';
+const selectorConstructorElementText = '.constructor-element__text';
+const selectorConstructorElementRow = 'span.constructor-element__row';
 const modals = '#modals';
+const selectorModalsH3 = `${modals} h3`;
+const selectorModalsButtonFirst = `${modals} button:first-of-type`;
+const selectorModalsOverlay = `${modals}>div:nth-of-type(2)`;
+const selectorModalsH2First = `${modals} h2:first-of-type`;
 
 describe('Тест e2e :: stellar-burger', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
-    cy.visit(TEST_URL);
+    cy.visit('/');
   });
   it('Переход на главную страницу', () => {
-    cy.visit(TEST_URL);
+    cy.visit('/');
   });
 
   it('Ингредиенты отображаются на странице', () => {
     cy.get(dataCyBun).should('have.length.at.least', 1);
-    cy.get('[data-cy="main"], [data-cy="sauce"]').should(
-      'have.length.at.least',
-      1
-    );
+    cy.get(`${dataCyMain}, ${dataCySauce}`).should('have.length.at.least', 1);
   });
 
   describe('Тест :: добавление ингредиентов в конструктор', () => {
     it('Добавление ингредиентов в конструктор и проверка названий', () => {
-      cy.get('[data-cy="bun"]:first-of-type')
+      cy.get(dataCyBunFirst)
         .find('p.text_type_main-default')
         .invoke('text')
         .then((bunName) => {
-          cy.get('[data-cy="bun"]:first-of-type button').click();
-
-          cy.get('div.constructor-element_pos_top')
-            .find('.constructor-element__text')
+          cy.get(`${dataCyBunFirst} button`).click();
+          cy.get(selectorConstructorElementTop)
+            .find(selectorConstructorElementText)
             .should('contain.text', bunName.trim());
         });
 
-      cy.get('[data-cy="main"]:first-of-type')
+      cy.get(dataCyMainFirst)
         .find('p.text_type_main-default')
         .invoke('text')
         .then((mainName) => {
-          cy.get('[data-cy="main"]:first-of-type button').click();
-
-          cy.get('span.constructor-element__row')
+          cy.get(`${dataCyMainFirst} button`).click();
+          cy.get(selectorConstructorElementRow)
             .eq(1)
-            .find('.constructor-element__text')
+            .find(selectorConstructorElementText)
             .should('contain.text', mainName.trim());
         });
 
-      cy.get('[data-cy="sauce"]:first-of-type')
+      cy.get(dataCySauceFirst)
         .find('p.text_type_main-default')
         .invoke('text')
         .then((sauceName) => {
-          cy.get('[data-cy="sauce"]:first-of-type button').click();
-
-          cy.get('span.constructor-element__row')
+          cy.get(`${dataCySauceFirst} button`).click();
+          cy.get(selectorConstructorElementRow)
             .eq(2)
-            .find('.constructor-element__text')
+            .find(selectorConstructorElementText)
             .should('contain.text', sauceName.trim());
         });
     });
@@ -72,7 +76,7 @@ describe('Тест e2e :: stellar-burger', () => {
           .then((ingredientName) => {
             cy.get(dataCyBunFirst).click();
             cy.get(modals).children().should('have.length', 2);
-            cy.get(`${modals} h3`)
+            cy.get(selectorModalsH3)
               .eq(1)
               .invoke('text')
               .then((modalIngredientName) => {
@@ -98,14 +102,14 @@ describe('Тест e2e :: stellar-burger', () => {
     describe('Тест :: различные способы закрытия модального окна', () => {
       it('Закрытие модального окна по крестику', () => {
         cy.get(dataCyBunFirst).click();
-        cy.get(`${modals} button:first-of-type`).click();
+        cy.get(selectorModalsButtonFirst).click();
         cy.wait(1000);
         cy.get(modals).children().should('have.length', 0);
       });
 
       it('Закрытие модального окна по клику на оверлей', () => {
         cy.get(dataCyBunFirst).click();
-        cy.get(`${modals}>div:nth-of-type(2)`).click({ force: true });
+        cy.get(selectorModalsOverlay).click({ force: true });
         cy.wait(1000);
         cy.get(modals).children().should('have.length', 0);
       });
@@ -119,22 +123,22 @@ describe('Тест e2e :: stellar-burger', () => {
       cy.intercept('GET', 'api/auth/user', { fixture: 'user' });
       cy.intercept('POST', 'api/orders', { fixture: 'order' });
       cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
-      cy.visit(TEST_URL);
+      cy.visit('/');
     });
 
     it('Оформление заказа с выбранными ингредиентами', () => {
       cy.get(dataCyOrder).should('be.disabled');
       cy.get(`${dataCyBunFirst} button`).click();
       cy.get(dataCyOrder).should('be.disabled');
-      cy.get('[data-cy="main"]:first-of-type button').click();
+      cy.get(`${dataCyMainFirst} button`).click();
       cy.get(dataCyOrder).should('be.enabled');
       cy.get(dataCyOrder).click();
       cy.get(modals).children().should('have.length', 2);
-      cy.get(`${modals} h2:first-of-type`).should(
+      cy.get(selectorModalsH2First).should(
         'have.text',
         orderFixture.order.number
       );
-      cy.get(`${modals} button:first-of-type`).click();
+      cy.get(selectorModalsButtonFirst).click();
       cy.get(dataCyOrder).children().should('have.length', 0);
       cy.get(dataCyOrder).should('be.disabled');
     });
